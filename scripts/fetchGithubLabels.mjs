40e4a7fd2e377048ab4589792ba47f9cb4cd119a
fill-the-fill <fill.ickevics@gmail.com>
2023-11-08 17:12:35 +0200

    Add fetching labels script

---
+++ scripts/fetchGithubLabels.mjs
@@ -0,0 +1,46 @@
+import fetch from 'node-fetch'
+import dotenv from 'dotenv'
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { writeFileSync, existsSync, mkdirSync } from 'fs';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+dotenv.config({ path: '.env.local' })
+
+const repo = 'cardano-foundation/CIPs'
+const token = process.env.GITHUB_TOKEN
+
+async function fetchGithubLabels() {
+    const repo = 'cardano-foundation/CIPs'
+    const token = process.env.GITHUB_TOKEN
+    const url = `https://api.github.com/repos/${repo}/labels`
+
+    const response = await fetch(url, {
+        headers: {
+            Authorization: `token ${token}`,
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch labels: ${response.status} ${response.statusText}`)
+    }
+
+    const labels = await response.json()
+    
+    const modifiedLabels = labels.map(({ name, color, description }) => ({ name, color, description }))
+
+    const labelsFolderPath = join(__dirname, '..', 'app', 'Labels')
+    const labelsFilePath = join(labelsFolderPath, 'labels.json')
+
+    if (!existsSync(labelsFolderPath)) {
+        mkdirSync(labelsFolderPath, { recursive: true })
+    }
+
+    writeFileSync(labelsFilePath, JSON.stringify(modifiedLabels, null, 2))
+
+    return modifiedLabels
+}
+
+fetchGithubLabels()
