aba8997b5b01fd6465ede1ab0a06f12be3a494fd
fill-the-fill <fill.ickevics@gmail.com>
2023-11-23 17:55:28 +0200

    add authors.json script

---
+++ scripts/createAuthorsArray.mjs
@@ -0,0 +1,67 @@
+import fs from 'fs';
+import path from 'path';
+
+function getMarkdownFiles(dirPath, arrayOfFiles) {
+  const files = fs.readdirSync(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  files.forEach((file) => {
+    if (fs.statSync(dirPath + "/" + file).isDirectory()) {
+      arrayOfFiles = getMarkdownFiles(dirPath + "/" + file, arrayOfFiles);
+    } else if (file === 'page.md') {
+      arrayOfFiles.push(path.join(dirPath, "/", file));
+    }
+  });
+
+  return arrayOfFiles;
+}
+
+function extractAuthors(filePath) {
+  const content = fs.readFileSync(filePath, 'utf-8');
+  const parts = content.split('---');
+  const headerLines = parts[1].trim().split('\n');
+
+  let lastKey = '';
+  const header = headerLines.reduce((obj, line) => {
+    if (line.startsWith('  ')) {
+      obj[lastKey] += ' ' + line.trim();
+    } else {
+      const [key, ...value] = line.split(':');
+      lastKey = key.trim();
+      obj[lastKey] = value.join(':').trim();
+    }
+    return obj;
+  }, {});
+
+  if (header.Authors) {
+    return header.Authors.split(/,|-/).flatMap(author => {
+      const match = author.match(/(.*)<(.*)>/);
+      if (match) {
+        const name = match[1].trim();
+        const email = match[2].trim();
+        return {
+          name,
+          email,
+        };
+      }
+      return [];
+    });
+  }
+
+  return [];
+}
+
+const markdownFiles = getMarkdownFiles('./content');
+let authors = [];
+
+markdownFiles.forEach((file) => {
+  const newAuthors = extractAuthors(file);
+  newAuthors.forEach((newAuthor) => {
+    if (!authors.find(author => author.name === newAuthor.name && author.email === newAuthor.email)) {
+      authors.push(newAuthor);
+    }
+  });
+});
+
+fs.writeFileSync('./data/authors.json', JSON.stringify(authors, null, 2));
