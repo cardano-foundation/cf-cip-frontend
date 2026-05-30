4e905df50dee41ee14a1800bd23186d061255215
rphair <rphair@cosd.com>
2026-05-29 21:53:05 -0400

    remove Adam Dean from editors list

--- app/contributors/page.tsx
+++ app/contributors/page.tsx
@@ -25,10 +25,6 @@ export default function ContributorsPage() {
       github_link: 'https://github.com/Ryun1',
     },
     {
-      name: 'Adam Dean',
-      github_link: 'https://github.com/Crypto2099',
-    },
-    {
       name: 'Thomas Vellekoop',
       github_link: 'https://github.com/perturbing',
     },
